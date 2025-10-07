1ef0ffe1152b4c6a7233729bc272f70dfe6b3271
ilyamak04 <makilya04@gmail.com>
2025-10-07 16:54:13 +0300

    add logger

--- cmd/k8s-panel/main.go
+++ cmd/k8s-panel/main.go
@@ -1,5 +1,41 @@
 package main
 
+import (
+	"fmt"
+	"log/slog"
+	"os"
+
+	"github.com/ilyamak04/k8s-panel/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
 func main() {
-	// TODO: fill me
+	cfg := config.MustLoad()
+
+	fmt.Println(cfg)
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+	return log
 }
